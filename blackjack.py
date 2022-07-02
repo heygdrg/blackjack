1a70ec9376d6e0bdd7aee515782e33ffdc240794
sovietor <94129991+heygdrg@users.noreply.github.com>
2022-07-02 20:50:39 +0200

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,6 @@
 import random
 import time
-
+time.sleep(1)
 #card = random.randint(1, 21)
 #FUTUR UPDATE PSEUDO
 #METTRE MODE SOLITAIRE AVEC UNE I.A1
@@ -14,17 +14,24 @@ player = input("how many player are playing :")
 if player == "1":
 
     if player == "1":
-    #while player == "1":
+
+        time.sleep(1)
         print("you must be 2 to play")
         ia = str(input("Do you wanna play solo [y/n]:"))
-
         if ia == "n":
 
             while ia == "n":
                 print("you must be 2 to play")
-                player = input("how many player are playing :")
                 time.sleep(1)
                 ia = str(input("Do you wanna play solo [y/n]:"))
+
+                if ia == "n":
+                    player = input("how many player are playing :")
+                    time.sleep(1)
+                else:
+                    input()
+
+                time.sleep(1)
 
         if ia == "y":
             print("downloading IA please wait")
@@ -42,14 +49,17 @@ if player == "1":
             difficulty = int(input("enter the difficulty :"))
 
             if difficulty == "1":
+                time.sleep(1)
                 print("you choose easy ")
                 difficulty = 1
 
             if difficulty == "2":
+                time.sleep(1)
                 print("you choose medium")
                 difficulty = 2
 
             if difficulty == "3":
+                time.sleep(1)
                 print("you choose hard")
                 difficulty = 3
 
@@ -57,6 +67,7 @@ if player == "1":
 
     number_player1 = random.randint(1, 21)
     IA_player1 = random.randint(1, 21)
+    time.sleep(1)
     print("player 1 have ", number_player1)
     time.sleep(2)
     print("IA have", IA_player1)
@@ -64,40 +75,43 @@ if player == "1":
 
 #DEUXIEME TIRAGE IA
 
-print("IA goes first")
-time.sleep(2)
+    print("IA goes first")
+    time.sleep(2)
 
 #IA rejoue car <15
 
-if IA_player1 < 15:
-    IA_player1 = IA_player1 + random.randint(1, 21)
-    print("IA have", IA_player1,)
+    if IA_player1 < 15:
+        IA_player1 = IA_player1 + random.randint(1, 21)
+        time.sleep(1)
+        print("IA have", IA_player1,)
 
 #IA ne rejoue pas >15
 
-if IA_player1 > 15:
-    print("IA dont play again too risky")
-    time.sleep(2)
+    if IA_player1 > 15:
+        print("IA dont play again too risky")
+        time.sleep(2)
 
-if IA_player1 > 21:
-    print("IA loose let's see if you do it better")
+    if IA_player1 > 21:
+        time.sleep(1)
+        print("IA loose let's see if you do it better")
 
-else:
-    print("IA finish with", IA_player1,)
+    else:
+        time.sleep(1)
+        print("IA finish with", IA_player1,)
 
 #DEUXIEME TOUR JOUEUR
 
-print("your turn")
-time.sleep(2)
+    print("your turn")
+    time.sleep(2)
 
-print("you have", number_player1,)
-time.sleep(1)
-answer = str(input("do you wanna try to take another card [y/n] :"))
-
-if answer == "y":
-    number_player1 = random.randint(1, 21) + number_player1
+    print("you have", number_player1,)
     time.sleep(1)
-    print("you have now", number_player1,)
+    answer = str(input("do you wanna try to take another card [y/n] :"))
+
+    if answer == "y":
+        number_player1 = random.randint(1, 21) + number_player1
+        time.sleep(1)
+        print("you have now", number_player1,)
 
     if number_player1 > 21:
         time.sleep(1)
@@ -109,30 +123,35 @@ if answer == "y":
         time.sleep(1)
         print("gg you you finish with", number_player1)
 
-if answer == "n":
-    time.sleep(1)
-    if number_player1 > 21:
+    if answer == "n":
         time.sleep(1)
-        print("sorry you have more than 21")
-        time.sleep(1)
-        print("you loose")
+        if number_player1 > 21:
+            time.sleep(1)
+            print("sorry you have more than 21")
+            time.sleep(1)
+            print("you loose")
 
-    if number_player1 < 21:
-        time.sleep(1)
-        print("gg you you finish with", number_player1)
+        if number_player1 < 21:
+            time.sleep(1)
+            print("gg you you finish with", number_player1)
 
-if IA_player1 < 21 and number_player1 < 21:
+    if IA_player1 < 21 and number_player1 < 21:
 
-    if IA_player1 < number_player1:
-        print("gg you win the game")
+        if IA_player1 < number_player1:
+            time.sleep(1)
+            print("gg you win the game")
+            input()
 
-    if IA_player1 > number_player1:
-        print("sorry you didn't win the match")
+        if IA_player1 > number_player1:
+            time.sleep(1)
+            print("sorry you didn't win the match")
+            input()
 
-    if IA_player1 == number_player1:
-        print("there an egality you must go to goulag now")
+        if IA_player1 == number_player1:
+            time.sleep(1)
+            print("there an egality you must go to goulag now")
 
-input("enter to close")
+
 
 
 #partie de 2 personnes
@@ -154,44 +173,70 @@ if player == "2":
 
     number_player1 = random.randint(1, 21)
     number_player2 = random.randint(1, 21)
+    time.sleep(1)
     print("player 1 have ", number_player1)
     time.sleep(1)
     print("player 2 have", number_player2)
 
 #deuxieme tirage joueur 1
 
+    time.sleep(1)
     print("turn to 1")
-    second_try_1 = str(input(f"you have", {number_player1}, "do you wanna take a second card [y/n]:"))
+    time.sleep(1)
+    print(psd_1, "you have ", number_player1)
+    time.sleep(1)
+    second_try_1 = str(input("do you wanna take a second card [y/n]:"))
 
     if second_try_1 == "y":
         number2_player1 = random.randint(1, 21)
         final_number1 = number2_player1 + number_player1
+        time.sleep(1)
         print("you have",final_number1)
 
         if final_number1 > 21:
             difference = final_number1 - 21
+            time.sleep(1)
             print("sorry you have",final_number1 ,"more than 21")
+            win2 = 2
 
         if final_number1 < 21:
-            print("gg player 1 you still in course")
-
+            time.sleep(1)
+            print("gg ", psd_1," you still in course")
+            win1 = 1
 # deuxieme tirage joueur 2
 
+    time.sleep(1)
     print("turn to 2")
-    second_try_2 = str(input("you have", number_player2, "do you wanna take a second card [y/n]:"))
+    time.sleep(1)
+    print(psd_2, "you have ", number_player2)
+    second_try_2 = str(input("do you wanna take a second card [y/n]:"))
+
 
     if second_try_2 == "y":
         number2_player2 = random.randint(1, 21)
         final_number2 = number2_player2 + number_player2
+        time.sleep(1)
         print("you have", final_number2)
 
         if final_number2 > 21:
             difference2 = final_number2 - 21
+            time.sleep(1)
             print("sorry you have", difference2, "more than 21")
+            win2 = 0
 
         if final_number2 < 21:
+            time.sleep(1)
             print("gg", psd_2, "you finish with", final_number2)
+            win2 = 1
 
+    if  final_number2 > final_number1 > 21:
+        print("gg", psd_1,"you just win")
+
+    if final_number1 > final_number2 > 21:
+        print("gg", psd_2, "you win")
+
+    if final_number1 == final_number2:
+        print("equality there's no winner")
 
 #partie de 3 personnes
 
